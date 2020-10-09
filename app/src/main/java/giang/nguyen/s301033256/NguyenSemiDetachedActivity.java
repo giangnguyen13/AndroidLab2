53f06a96e5873939262d269b91e90f575dde4b6b
Giang Nguyen <giangkawaii1999@gmail.com>
2020-10-08 21:34:20 -0400

    Add layout + activity for house type

--- app/src/main/java/giang/nguyen/s301033256/NguyenSemiDetachedActivity.java
+++ app/src/main/java/giang/nguyen/s301033256/NguyenSemiDetachedActivity.java
@@ -3,21 +3,19 @@ package giang.nguyen.s301033256;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class NguyenActivitySelectHouse extends AppCompatActivity {
+public class NguyenSemiDetachedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_house);
+        setContentView(R.layout.activity_nguyen_semi_detached);
     }
 
     @Override
