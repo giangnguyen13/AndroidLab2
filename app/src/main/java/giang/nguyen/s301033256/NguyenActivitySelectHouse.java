9b01ca80daf031bd3a0af5f3e3240ee1a40cac08
Giang Nguyen <giangkawaii1999@gmail.com>
2020-10-08 20:49:36 -0400

    main screen change + add house selection view

---
+++ app/src/main/java/giang/nguyen/s301033256/NguyenActivitySelectHouse.java
@@ -0,0 +1,20 @@
+package giang.nguyen.s301033256;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class NguyenActivitySelectHouse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_house);
+    }
+}
